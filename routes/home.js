a33cac45fe7168e50c307bc1ca0b186f95638bb1
chauncy-n <eldoon@comcast.net>
2018-10-24 13:58:12 -0700

    working on home link

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-// router.get('/', function(req, res, next){
-//     res.render('index', home.index);
-// });
+router.get('/', function(req, res, next){
+    res.sendFile(__dirname, home.index);
+});
